cc97177036ac06a5b8cb9b89ca75ba5ead9475b5
StephonKumar2000 <56920459+StephonKumar2000@users.noreply.github.com>
2021-12-20 15:30:08 -0500

    adding student front

--- src/pages/AddStudent/AddStudent.jsx
+++ src/pages/AddStudent/AddStudent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import'bootstrap/dist/css/bootstrap.css';
-
+//
 const AddStudent = (props) => {
   console.log(props.path);
   return (
