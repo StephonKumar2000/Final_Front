327e10cd386cb51c4c1ae72e80b28f6c5e2d26e6
StephonKumar2000 <56920459+StephonKumar2000@users.noreply.github.com>
2021-12-20 15:30:32 -0500

    adding campusus.jsx front

--- src/pages/AllCampuses/Campuses.jsx
+++ src/pages/AllCampuses/Campuses.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import CampusesArray from "./components/CampusArray/CampusesArray";
 import'bootstrap/dist/css/bootstrap.css';
-
+//
 const Campuses = () => {
   return (
     <div className="campuses">
